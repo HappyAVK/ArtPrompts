0aa099813bb2ee18a521f31409596e8fdfac0f9a
Alex <alexlsouthall@yahoo.com>
2022-11-25 15:40:08 +0000

    Created a simple GUI

--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
-def get_prompts(filepath="promts.txt"):
+def get_prompts(filepath="prompts.txt"):
     """HNNNNNG AQUA CUTE CUTE CUTE"""
     with open(filepath, "r") as file:
         local_prompts = file.readlines()
     return local_prompts
 
 
-def write_prompts(aprompts_arg, filepath="promts.txt"):
+def write_prompts(aprompts_arg, filepath="prompts.txt"):
     with open(filepath, "w") as file:
         file.writelines(aprompts_arg)
