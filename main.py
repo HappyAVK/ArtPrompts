ac00af8d0c006ebb500bed4d34b830597112593a
Alex <alexlsouthall@yahoo.com>
2022-11-26 14:16:49 +0000

    Added an 'EDIT PROMPT' and 'Prompt List' button to the GUI, changed layout of GUI

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import functions
 import PySimpleGUI as GUI
 
 label = GUI.Text("Type in an Art Prompt")
-input1 = GUI.InputText(tooltip="Enter a Prompt!",key="prompt")
+input1 = GUI.InputText(tooltip="Enter a Prompt!",key="n_prompt")
 add_button = GUI.Button("Add")
+edit_button =GUI.Button("Edit")
+list_box = GUI.Listbox(values=functions.get_prompts(), key="prompts",
+                       enable_events=True, size=[45, 10])
+
+
 W = GUI.Window('Art Prompts App',
-               layout=[[label], [input1], [add_button]],
+               layout=[[label, edit_button], [input1], [add_button],[list_box]],
                font=('Helvetica', 20))
 while True:
     event, value = W.read()
@@ -14,9 +19,22 @@ while True:
     match event:
         case "Add":
             APrompts = functions.get_prompts()
-            new_prompt = value["prompt"] +'\n'
+            new_prompt = value["n_prompt"] + '\n'
             APrompts.append(new_prompt)
             functions.write_prompts(APrompts)
+            W['prompts'].update(values=APrompts)
+        case "Edit":
+            prompt_edit = value["prompts"][0]
+            new_prompt = value["n_prompt"] + '\n'
+
+            APrompts = functions.get_prompts()
+            index = APrompts.index(prompt_edit)
+            APrompts[index] = new_prompt
+            functions.write_prompts(APrompts)
+            W['prompts'].update(values=APrompts)
+        case "prompts":
+            W['n_prompt'].update(value=value["prompts"][0])
+
         case GUI.WIN_CLOSED:
             break
 
