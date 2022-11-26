60cdb7b32f2b2d716d5abf46cccb6a7895054145
Alex <alexlsouthall@yahoo.com>
2022-11-26 13:33:01 +0000

    Added an 'Add prompt' button to the GUI

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ import functions
 import PySimpleGUI as GUI
 
 label = GUI.Text("Type in an Art Prompt")
-input1 = GUI.InputText(tooltip="Enter a Prompt!")
+input1 = GUI.InputText(tooltip="Enter a Prompt!",key="prompt")
 add_button = GUI.Button("Add")
-W = GUI.Window('Art Prompts App',layout=[[label], [input1], [add_button]])
+W = GUI.Window('Art Prompts App',
+               layout=[[label], [input1], [add_button]],
+               font=('Helvetica', 20))
+while True:
+    event, value = W.read()
+    print(event)
+    print(value)
+    match event:
+        case "Add":
+            APrompts = functions.get_prompts()
+            new_prompt = value["prompt"] +'\n'
+            APrompts.append(new_prompt)
+            functions.write_prompts(APrompts)
+        case GUI.WIN_CLOSED:
+            break
 
-W.read()
 W.close()
