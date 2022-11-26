4c10ac9a02f108196193797d8e5f279b132c6a51
Alex <alexlsouthall@yahoo.com>
2022-11-26 12:22:11 +0000

    Push Commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import functions
 import PySimpleGUI as GUI
 
-label=GUI.Text("Type in an Art Prompt")
-input= GUI.InputText(tooltip="Enter a Prompt!")
+label = GUI.Text("Type in an Art Prompt")
+input1 = GUI.InputText(tooltip="Enter a Prompt!")
 add_button = GUI.Button("Add")
-W = GUI.Window('Art Prompts App',layout=[[label], [input], [add_button]])
+W = GUI.Window('Art Prompts App',layout=[[label], [input1], [add_button]])
 
 W.read()
 W.close()
