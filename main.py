c25df195ea0b286f6e786783516a6d587fd56c38
Alex <alexlsouthall@yahoo.com>
2022-11-28 14:35:14 +0000

    Added an 'Complete PROMPT' and 'EXIT' button to the GUI.

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import functions
 import PySimpleGUI as GUI
 
 label = GUI.Text("Type in an Art Prompt")
-input1 = GUI.InputText(tooltip="Enter a Prompt!",key="n_prompt")
+input1 = GUI.InputText(tooltip="Enter a Prompt!", key="n_prompt")
 add_button = GUI.Button("Add")
-edit_button =GUI.Button("Edit")
+edit_button = GUI.Button("Edit")
+complete_button = GUI.Button("Complete")
 list_box = GUI.Listbox(values=functions.get_prompts(), key="prompts",
-                       enable_events=True, size=[45, 10])
-
+                       enable_events=True, size=(45, 10))
+exit_button = GUI.Button("Exit")
 
 W = GUI.Window('Art Prompts App',
-               layout=[[label, edit_button], [input1], [add_button],[list_box]],
+               layout=[[label, edit_button, complete_button],
+                       [input1],
+                       [add_button],
+                       [list_box],
+                       [exit_button]],
                font=('Helvetica', 20))
 while True:
     event, value = W.read()
@@ -19,7 +24,7 @@ while True:
     match event:
         case "Add":
             APrompts = functions.get_prompts()
-            new_prompt = value["n_prompt"] + '\n'
+            new_prompt = value["n_prompt"].strip("\n") + '\n'
             APrompts.append(new_prompt)
             functions.write_prompts(APrompts)
             W['prompts'].update(values=APrompts)
@@ -35,7 +40,17 @@ while True:
         case "prompts":
             W['n_prompt'].update(value=value["prompts"][0])
 
+        case "Complete":
+            to_complete_prompt = value["prompts"][0]
+            APrompts = functions.get_prompts()
+            APrompts.remove(to_complete_prompt)
+            functions.write_prompts(APrompts)
+            W['prompts'].update(values=APrompts)
+            W["n_prompt"].update(value="")
+
         case GUI.WIN_CLOSED:
+            break
+        case "Exit":
             break
 
 W.close()
