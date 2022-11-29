95c16032a8e7d2744e8b59247bc2b4c5705d734e
Alex <alexlsouthall@yahoo.com>
2022-11-29 15:15:11 +0000

    Added an 'Complete PROMPT' and 'EXIT' button to the GUI.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import functions
 import PySimpleGUI as GUI
-
+import time
+GUI.theme("DarkPurple4")
+clock = GUI.Text(key="clock")
 label = GUI.Text("Type in an Art Prompt")
 input1 = GUI.InputText(tooltip="Enter a Prompt!", key="n_prompt")
 add_button = GUI.Button("Add")
@@ -15,12 +17,13 @@ W = GUI.Window('Art Prompts App',
                        [input1],
                        [add_button],
                        [list_box],
-                       [exit_button]],
+                       [exit_button, clock]],
                font=('Helvetica', 20))
 while True:
-    event, value = W.read()
-    print(event)
-    print(value)
+    event, value = W.read(timeout=200)
+    W['clock'].update(value=time.strftime("%b %d, %Y, %H:%M:%S"))
+    #print(event)
+    #print(value)
     match event:
         case "Add":
             APrompts = functions.get_prompts()
@@ -29,24 +32,30 @@ while True:
             functions.write_prompts(APrompts)
             W['prompts'].update(values=APrompts)
         case "Edit":
-            prompt_edit = value["prompts"][0]
-            new_prompt = value["n_prompt"] + '\n'
+            try:
+                prompt_edit = value["prompts"][0]
+                new_prompt = value["n_prompt"] + '\n'
 
-            APrompts = functions.get_prompts()
-            index = APrompts.index(prompt_edit)
-            APrompts[index] = new_prompt
-            functions.write_prompts(APrompts)
-            W['prompts'].update(values=APrompts)
+                APrompts = functions.get_prompts()
+                index = APrompts.index(prompt_edit)
+                APrompts[index] = new_prompt
+                functions.write_prompts(APrompts)
+                W['prompts'].update(values=APrompts)
+            except IndexError:
+                GUI.popup("Select an Item Numbnuts!", font=10)
         case "prompts":
             W['n_prompt'].update(value=value["prompts"][0])
 
         case "Complete":
-            to_complete_prompt = value["prompts"][0]
-            APrompts = functions.get_prompts()
-            APrompts.remove(to_complete_prompt)
-            functions.write_prompts(APrompts)
-            W['prompts'].update(values=APrompts)
-            W["n_prompt"].update(value="")
+            try:
+                to_complete_prompt = value["prompts"][0]
+                APrompts = functions.get_prompts()
+                APrompts.remove(to_complete_prompt)
+                functions.write_prompts(APrompts)
+                W['prompts'].update(values=APrompts)
+                W["n_prompt"].update(value="")
+            except IndexError:
+                GUI.popup("You haven't selected anything to complete, Idiot", font=10)
 
         case GUI.WIN_CLOSED:
             break
